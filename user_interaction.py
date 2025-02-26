2df7495643ccc3e1ff10cfca20825bf49ca48468
Vladimir X-oc <vovaess@gmail.com>
2025-02-26 12:31:25 +0300

    simple iteration method

--- user_interaction.py
+++ user_interaction.py
@@ -1,4 +1,18 @@
-def get_matrix():
+def matrix_from_file():
+    while True:
+        try:
+            file_name = input("Введите название файла: ")
+            with open(file_name, 'r') as file:
+                lines = list(map(lambda x: x.rstrip('\n'), file.readlines()))
+                matrix = []
+                for line in lines:
+                    matrix.append(list(map(float, line.split())))
+                return matrix
+        except Exception:
+            print("Файл некорректен")
+
+
+def matrix_from_console():
     while True:
         try:
             n = int(input("Введите размер матрицы: "))
@@ -20,18 +34,36 @@ def get_matrix():
     return matrix
 
 
+def get_matrix():
+    source = ""
+    while source != "1" and source != "2":
+        source = input("Откуда будет вводиться матрица? 1 - консоль, 2 - файл ")
+    if source == "2":
+        return matrix_from_file()
+    return matrix_from_console()
+
+
 def get_accuracy():
     while True:
         try:
             a = float(input("Введите точность: "))
             if a > 0:
-                return
+                return a
             else:
                 print("Ввод некорректен!")
-        except ValueError:
+        except Exception:
             print("Ввод некорректен!")
 
 
 def print_matrix(matrix):
     for row in matrix:
-        print(*row, sep = '\t')
+        print(*row, sep='\t')
+
+
+def print_x_values(x_values, precision):
+    print(*list(map(lambda x: round(x, precision), x_values)))
+
+
+def print_errors(x_last, x_current):
+    errors = [abs(x_last[i] - x_current[i]) for i in range(len(x_last))]
+    print(*errors)
